8bab97afbf9903e478abf7a68dfa391fafec00ea
Oge Obubu <ogeobubu@gmail.com>
2022-01-01 08:09:50 +0100

    comment section added

--- day1/src/App.svelte
+++ day1/src/App.svelte
@@ -3,7 +3,7 @@
   import data from "../data.json"
 
 import Fa from 'svelte-fa/src/fa.svelte'
-import { faPlus, faMinus } from '@fortawesome/free-solid-svg-icons'
+import { faPlus, faMinus, faReply } from '@fortawesome/free-solid-svg-icons'
 
 let count = 0
 
@@ -19,13 +19,32 @@ let count = 0
             <button class="button" on:click={() => count += 1}>
               <Fa icon={faPlus} />
             </button>
-            <span>{count}</span>
+
+            <span class="counter-number">{count}</span>
+
             <button class="button" on:click={() => count -= 1}>
             <Fa icon={faMinus} />
             </button>
           </div>
 
-          <div class="card-main">Hello World</div>
+          <div class="card-main">
+            <div class="top">
+              <div class="profile">
+                <div class="image-container">
+                  <img src="../src/assets/images/avatars/image-amyrobson.png" slt="image" />
+                </div>
+                <h4 class="name">amyrobson</h4>
+                <span class="time">1 month ago</span>
+              </div>
+              <div class="reply">
+                <Fa style="color: #615d9d" icon={faReply} /> <span class="reply-text">Reply</span>
+              </div>
+            </div>
+
+            <div class="card-body">
+              Impressive! Though it seems the drag feature could be improved. But overall it looks incredible. You've nailed the design and the responsiveness at various breakpoints works really well.
+            </div>
+          </div>
           </div>
       </div>
     </div>
@@ -55,15 +74,14 @@ margin: 0;
   }
 
   .cards {
-    width: 450px;
-    margin: 0 auto;
+    width: 100%;
   }
 
   .card {
-    width: 450px;
+    width: 600px;
     padding: 1rem;
     background-color: hsl(0, 0%, 100%);
-
+    margin: 0 auto;
   }
 
   .flex {
@@ -79,9 +97,10 @@ margin: 0;
     flex-direction: column;
     justify-content: space-between;
     align-items: center;
+    margin-right: 1.5rem;
   }
 
-  span {
+  .counter-number {
     margin: 1rem 0;
   }
 
@@ -92,6 +111,64 @@ margin: 0;
 
   .fa {
     color: red;
+  }
+
+  .card-main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+  }
+
+  .top {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+
+  }
+
+  .profile {
+    display: flex;
+    align-items: center;
+
+  }
+
+  .time {
+    color: #9e9fa4;
+  }
+
+  .image-container {
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+    margin-right: 1rem;
+  }
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+
+  .name {
+    color: #969a9f;
+    margin-right: 1rem;
+  }
+
+  .reply {
+    justify-self: flex-end;
+    cursor: pointer;
+  }
+
+  .reply-text {
+    margin-left: .5rem;
+    color: #615d9d;
+  }
+
+  .card-body {
+    color: #c2c4c5;
+    margin-top: .6rem;
   }
 
 </style>
