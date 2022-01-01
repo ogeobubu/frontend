2ca3f664df1448d28839b5ff6324ac4dd41e2ae6
Oge Obubu <ogeobubu@gmail.com>
2022-01-01 06:19:01 +0100

    added styling globally

--- day1/src/App.svelte
+++ day1/src/App.svelte
@@ -1,60 +1,33 @@
 <script>
   import Counter from './lib/Counter.svelte'
+  import data from "../data.json"
+
 </script>
 
 <main>
-  <h1>Hello world!</h1>
-
-  <Counter />
-
-  
-  <!-- Pull dynamic content from the data.json file -->
-  
-  <div class="attribution">
-    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
-    Coded by <a href="#">Your Name Here</a>.
+  <div class="container">
+  Hello World..
+  </div>
 </main>
 
 <style>
-  :root {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  }
 
-  main {
-    text-align: center;
-    padding: 1em;
+:global(*) {
+@import url('https://fonts.googleapis.com/css2?family=Rubik:wght@400;500;700&display=swap');
+		margin: 0;
+    border: 0;
+    box-sizing: border-box;
+    background-color: hsl(239, 57%, 85%);
+    font-family: 'Rubik', sans-serif;
+    line-height: 1.6;
+	}
+
+  .container {
+    max-width: 1440px;
     margin: 0 auto;
+    width: 100%;
+    padding: 1rem;
+
   }
 
-  img {
-    height: 16rem;
-    width: 16rem;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4rem;
-    font-weight: 100;
-    line-height: 1.1;
-    margin: 2rem auto;
-    max-width: 14rem;
-  }
-
-  p {
-    max-width: 14rem;
-    margin: 1rem auto;
-    line-height: 1.35;
-  }
-
-  @media (min-width: 480px) {
-    h1 {
-      max-width: none;
-    }
-
-    p {
-      max-width: none;
-    }
-  }
 </style>
